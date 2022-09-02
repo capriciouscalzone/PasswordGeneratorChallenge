f54120c1775ffb2912240d375eb373436b050fb9
Michael Crandall <mec192@gmail.com>
2022-09-01 23:31:49 -0500

    started adding first prompst when you click button

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,7 @@ var btngenerateEl = document.querySelector("#generate");
 var btncopyEl = document.querySelector("#copy");
 
 
+btngenerateEl.addEventListener("click", function() {
+
+    var charLength = prompt("How many letters long should your new password be? choose a number between 8 and 128");
+})
